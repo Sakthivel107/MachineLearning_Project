3d40a2d3165530ffffbcbf47245c60b8d3ca3571
Sakthivel107 <sakthivel107g@gmail.com>
2024-11-09 07:53:30 +0530

    Data Ingestion

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-import logging
+from src.logger import logging
 def error_details(error,error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()
     file_name=exc_tb.tb_frame.f_code.co_filename
@@ -11,4 +11,4 @@ class MyException(Exception):
             super().__init__(error_message)
             self.error_message=error_details(error_message,error_detail=error_detail)
         def __str__(self) :
-            return self.error_message
+            return self.error_message     
