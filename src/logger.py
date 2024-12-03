e8faa8d9f017f54b085fa1f539dea796d6e3e2f8
Sakthivel <144320387+Sakthivel107@users.noreply.github.com>
2024-12-03 18:33:43 +0530

    Update logger.py

--- src/logger.py
+++ src/logger.py
@@ -2,7 +2,7 @@ import logging
 import os
 from datetime import datetime
 
-# Replace slashes with underscores for a valid filename
+
 LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
 logs_path = os.path.join(os.getcwd(), "logs")
 os.makedirs(logs_path, exist_ok=True)
